76ae0c004d49dd02bd7aac32aa023e70b99f2a57
Francisco Estupiñán <90729579+festupinans@users.noreply.github.com>
2021-11-28 20:24:42 -0500

    a

---
+++ lib/carritoCompra.dart
@@ -0,0 +1,174 @@
+import 'package:alpha/listaPersonas.dart';
+import 'package:alpha/main.dart';
+import 'package:alpha/registrarPedido.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class carritoCompras extends StatefulWidget {
+
+  final List<datosPedido> pedido;
+  final String id;
+  final String cedula;
+
+  const carritoCompras({required this.pedido, required this.cedula, required this.id});
+
+  @override
+  _carritoComprasState createState() => _carritoComprasState();
+}
+
+class _carritoComprasState extends State<carritoCompras> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Carrito de compras'),
+
+
+      ),
+      drawer: menu(),
+      body: Center(
+        child: ListView.builder(
+            itemCount: widget.pedido.length,
+            itemBuilder: (BuildContext context, i){
+              return ListTile(
+                  title: Text(widget.pedido[i].nombre+' - '+widget.pedido[i].descripcion+' - '+widget.pedido[i].precio+' - '+widget.pedido[i].cant.toString()+' - '+widget.pedido[i].total.toString()),
+                trailing: Icon(Icons.delete, size: 30, color: Colors.pink,),
+                onTap: (){
+                    widget.pedido.removeAt(i);
+                    setState(() {});
+                },
+              );
+              setState(() {});
+
+            }
+        ),
+      ),
+      bottomNavigationBar: carritoBar(listaPedido: widget.pedido, cliente: widget.cedula, negocio: widget.id,),
+    );
+  }
+}
+
+class carritoBar extends StatefulWidget {
+
+  final List<datosPedido> listaPedido;
+  final String negocio;
+  final String cliente;
+  const carritoBar({required this.listaPedido, required this.negocio, required this.cliente});
+
+
+
+  @override
+  _carritoBarState createState() => _carritoBarState();
+}
+
+
+
+class _carritoBarState extends State<carritoBar> {
+
+
+  void registrarDetalle(codP){
+    CollectionReference detalle = FirebaseFirestore.instance.collection('DetallePedido');
+    for(var dato=0; dato< widget.listaPedido.length; dato++){
+      detalle.add({
+        'pedido': codP,
+        'producto': widget.listaPedido[dato].codigo,
+        'cantidad': widget.listaPedido[dato].cant,
+        'total': widget.listaPedido[dato].total
+      });
+    }
+  }
+
+
+  void registrar(){
+    DateTime hoy = new DateTime.now();
+    DateTime fecha = new DateTime(hoy.year, hoy.month, hoy.day);
+    int totalP=0;
+    String codPedido='';
+    for(int i=0; i<widget.listaPedido.length; i++){
+      totalP+=widget.listaPedido[i].total;
+    }
+    CollectionReference pedidos = FirebaseFirestore.instance.collection('Pedidos');
+    pedidos.add({
+      'negocio': widget.negocio,
+      'cedula': widget.cliente,
+      'fecha': fecha,
+      'total': totalP
+    }).then((value) => registrarDetalle(value.id));
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    return BottomNavigationBar(
+      backgroundColor: Colors.cyan,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_shopping_cart_sharp, size: 30,),
+            label: 'Agregar\nCruso',
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_shopping_cart_sharp, size: 30,),
+            label: 'Total',
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_shopping_cart_sharp, size: 30,),
+            label: 'Confirmar\nCompra',
+          ),
+        ],
+      onTap: (indice){
+        if(indice==0){
+          Navigator.pop(context);
+        }else if(indice==1){
+          int totalPedido=0;
+          for(int i=0; i<widget.listaPedido.length; i++){
+            totalPedido+=widget.listaPedido[i].total;
+          }
+          // print('Total es: '+totalPedido.toString());
+          showDialog(
+              context: context, 
+              builder: (context)=>AlertDialog(
+                title: Text('Total de la compra: ',style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.pink),),
+                contentPadding: EdgeInsets.all(20.0),
+                content: Text(totalPedido.toString(),style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.pink),),
+              )
+          );
+        }else{
+          showDialog(
+              context: context,
+              builder: (context)=>AlertDialog(
+                title: Text('Confirmar la compra: ',style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.pink),),
+                contentPadding: EdgeInsets.all(20.0),
+                content: Text('Confirmar el registro de la compra: ',style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.pink),),
+                actions: [
+                  ElevatedButton(
+                      onPressed: (){
+                        registrar();
+                        Fluttertoast.showToast(msg: 'Pedido registrado exitosamente', toastLength: Toast.LENGTH_LONG, gravity: ToastGravity.TOP);
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>listaPesonas(cedula: widget.cliente)),
+                        );},
+
+                      child: Text('Confirmar')
+                  ),
+
+                  ElevatedButton(
+                      onPressed: (){
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('Cancelar')
+                  ),
+                ],
+              )
+          );
+        }
+      },
+    );
+  }
+}
--- lib/registrarPedido.dart
+++ lib/registrarPedido.dart
@@ -1,3 +1,4 @@
+import 'package:alpha/carritoCompra.dart';
 import 'package:alpha/main.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +17,8 @@ class _registrarPedidoState extends State<registrarPedido> {
 
   List listaCursos=[];
   List codigoGustos=[];
+  List<datosPedido> pedido=[];
+  int cont=0;
 
   void initState(){
     super.initState();
@@ -25,11 +28,11 @@ class _registrarPedidoState extends State<registrarPedido> {
   Future<void> getCursos() async{
     CollectionReference gustos = FirebaseFirestore.instance.collection('Likes');
     String cod="";
-    QuerySnapshot cursos = await gustos.where('People', isEqualTo: widget.id).get();
+    QuerySnapshot cursos = await gustos.where('persona', isEqualTo: widget.id).get();
+
     if(cursos.docs.length>0){
       for(var doc in cursos.docs){
         cod=doc.id.toString();
-        print(cod);
         codigoGustos.add(cod);
         listaCursos.add(doc.data());
       }
@@ -44,7 +47,21 @@ class _registrarPedidoState extends State<registrarPedido> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Registrar pedido'),
+        actions: [
+          IconButton(
+              onPressed: (){
+                // for(var i=0; i<pedido.length; i++){
+                //   print(pedido[i].precio);
+                //   print(pedido[i].cant);
+                //   print(pedido[i].total);
+                // }
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>carritoCompras(pedido: pedido, cedula: widget.cedula, id: widget.id)));
 
+
+              },
+              icon: Icon(Icons.add_shopping_cart, size: 30, color: Colors.amber,)
+          ),
+        ],
       ),
       drawer: menu(),
       body:
@@ -58,16 +75,51 @@ class _registrarPedidoState extends State<registrarPedido> {
       ListView.builder(
           itemCount: listaCursos.length,
           itemBuilder: (BuildContext context, i){
+            var can= TextEditingController();
             return ListTile(
+              leading: Icon(Icons.add_box, size: 30, color: Colors.amber),
               title: Container(
                 padding: EdgeInsets.all(20.0),
                 color: Colors.pink,
                 child: Text(listaCursos[i]['nombre']+' '+listaCursos[i]['descripcion']+' '+listaCursos[i]['precio'].toString()),
               ),
+              subtitle: TextField(
+                controller: can,
+                decoration: InputDecoration(
+                  hintText: 'Cantidad'
+                ),
+              ),
+              onTap: (){
+                if(can.text.isEmpty){
+                  can.text='0';
+                }
+                
+                int total= int.parse(can.text)*(int.parse(listaCursos[i]['precio'].toString()));
+
+                datosPedido p = datosPedido(codigoGustos[i], listaCursos[i]['nombre'], listaCursos[i]['descripcion'], listaCursos[i]['precio'].toString(), int.parse(can.text), total);
+
+                pedido.add(p);
+                
+                // print('Cantidad: '+can.text);
+                // print('Precio: '+listaCursos[i]['precio'].toString());
+                // print(int.parse(can.text)*(int.parse(listaCursos[i]['precio'].toString())));
+              },
             );
           }
       ),
 
     );
   }
+}
+
+
+class datosPedido{
+  String codigo='';
+  String nombre='';
+  String descripcion='';
+  String precio='';
+  int cant=0;
+  int total=0;
+
+  datosPedido(this.codigo, this.nombre, this.descripcion, this.precio, this.cant, this.total);
 }
